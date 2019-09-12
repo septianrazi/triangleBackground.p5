98ffd94a1a5613aab1e96e56f0cc2ad55f2813fb
Septian Razi <u6086829@anu.edu.au>
2019-09-12 11:13:49 +1000

    in progress to make randomised things

--- sketch.js
+++ sketch.js
@@ -1,8 +1,77 @@
 function setup() {
+  // create the canvas
   createCanvas(windowWidth, windowHeight);
+  background(0,0,0);
 
 }
 
+var coords =  [[1,3],[2,4],[3,5],[2,6],[1,7]];
+var xLength = 80;
+var yLength = 50;
+//variables used to show squares size based on previous variables.
+var squareWidth;
+var squareHeight;
+
 function draw() {
+  // variables for how many pixels wanted
+  background(0);
+  noStroke();
+
+
+  //variables used to show squares size based on previous variables.
+  squareWidth = width/xLength;
+  squareHeight = height/yLength;
+  //array in array to colour coordinates for name and >
+  // x values are 1 over
+
+
+
+  // for loop to colour in pixels for name and >
+  for (cor = 0 ; cor <coords.length; cor++){
+    fill(0,coords[cor][2],0);
+    rect (0+(coords[cor][0])*squareWidth,0+(coords[cor][1]-1)*squareHeight,
+    squareWidth,squareHeight);
+  }
+
+}
+
+
+
+function mouseMoved(){
+    x = Math.round(mouseX/squareWidth)
+    y = Math.round(mouseY/squareHeight)
+
+
+  coords.push([x,y])
+
+  iterations= randomGaussian(10,5);
+
+  if ((mouseX % squareWidth) < 2){
+    coords = [[x,y]];
+    for (i = 0; i < iterations; i++){
+      minX = Math.round(randomGaussian(0, 1));
+      minY = Math.round(randomGaussian(0, 1));
+      col = Math.round(random(150,190));
+
+
+      console.log(minX)
+      coords.push([x+minX, y+minY, col])
+
+      minX = Math.round(randomGaussian(0, 2));
+      minY = Math.round(randomGaussian(0, 2));
+      col = Math.round(random(30,70));
+
+      console.log(minX)
+      coords.push([x+minX, y+minY, col])
+
+      minX = Math.round(randomGaussian(0, 3));
+      minY = Math.round(randomGaussian(0, 3));
+      col = Math.round(random(0,20));
+
+      console.log(minX)
+      coords.push([x+minX, y+minY, col])
+    }
+
+  }
   
 }
